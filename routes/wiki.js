6120f4eb77a56c9dfe63f674ef477463107e957c
Vanessa Sergeon <vsergeon@gmail.com>
2014-08-14 16:16:25 -0400

    Add markdown support to app.

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models/');
+// var marked = require('marked');
 
 router.get('/:url_name', function(req, res) {
   //we can use req.param.page_title
   //to find from the db
-  //then asyncrouuslllllllly call render
-  // {Page: [mongoose model], User: [mongoose model]}
   models.Page.findOne({url_name: req.params.url_name}, function(err, page) {
     res.render('show', {page: page});
   });
